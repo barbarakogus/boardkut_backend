fd19974a090c2dbd7323aa7f399d93c6fba417b8
Bárbara Kógus <barbarakogus@gmail.com>
2024-12-04 14:07:26 +0100

    wip

--- data/repository.js
+++ data/repository.js
@@ -5,6 +5,7 @@ module.exports = {
     const res = await SUPABASE.from('boardgames').select();
     return { boardgames: res.data, error: res.error?.message };
   },
+
   getBoardgameById: async (id) => {
     const res = await SUPABASE.from('boardgames').select().eq('id', id);
     if (!res) return null;
@@ -13,6 +14,7 @@ module.exports = {
     }
     return res.data[0];
   },
+
   addBoardgame: async (data) => {
     const res = await SUPABASE.from('boardgames').insert({ title: data.title, type: data.type, players: data.players, play_time: data.playTime, play_time_unit: data.playTimeUnit, language: data.language, purchase_date: data.purchaseDate }).select()
     if (!res || res.length === 0) {
@@ -21,10 +23,12 @@ module.exports = {
       return res.data[0];
     }
   },
+
   updateBoardgame: async (data, id) => {
     const res = await SUPABASE.from('boardgames').update({ title: data.title, type: data.type, players: data.players, play_time: data.playTime, play_time_unit: data.playTimeUnit, language: data.language, age: data.age, purchase_date: data.purchaseDate }).eq('id', id)
     return res.data;
   },
+  
   deleteBoardgame: async (id) => {
     const res = await SUPABASE.from('boardgames').delete().eq('id', id)
     return res.data;
